53b9fb5cc7ba121e494ebb00d46c510d1f8ea42e
sbuettner <simon.buettner@moovel.com>
2015-09-22 11:33:02 +0200

    Add apache license header.

--- logspoutkinesis.go
+++ logspoutkinesis.go
@@ -1,6 +1,23 @@
-// Based on:
-// - https://github.com/rtoma/logspout-redis-logstash/blob/master/redis.go
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+/* Based on:
+ * https://github.com/rtoma/logspout-redis-logstash/blob/master/redis.go
+*/
+ 
 package logspoutkinesis
 
 import (
