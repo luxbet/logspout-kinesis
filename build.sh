0c92534b97c25d9d30ca48fa8496cb1b6d49398d
Oliver Wahlen <owahlen@googlemail.com>
2015-10-09 10:19:15 +0200

    attempt to fix pump bug

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cat > ./Dockerfile <<DOCKERFILE
-FROM gliderlabs/logspout:master
+FROM gliderlabs/logspout:latest
 DOCKERFILE
 
 cat > ./modules.go <<MODULES
 package main
 import (
-  _ "github.com/gliderlabs/logspout/httpstream"
+//  _ "github.com/gliderlabs/logspout/httpstream"
   _ "github.com/gliderlabs/logspout/routesapi"
   _ "github.com/moovel/logspout-kinesis"
 )
