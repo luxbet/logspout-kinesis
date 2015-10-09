44934f04a276a399ae2cb958727a848edc6749df
Oliver Wahlen <owahlen@googlemail.com>
2015-10-09 13:34:55 +0200

    re-added httpstream

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ DOCKERFILE
 cat > ./modules.go <<MODULES
 package main
 import (
-//  _ "github.com/gliderlabs/logspout/httpstream"
+  _ "github.com/gliderlabs/logspout/httpstream"
   _ "github.com/gliderlabs/logspout/routesapi"
   _ "github.com/moovel/logspout-kinesis"
 )
